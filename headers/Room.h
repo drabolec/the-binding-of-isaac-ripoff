cecc231394abb20c2958c07688b6bcb927311c55
Jakub <kubadzi05@gmail.com>
2025-06-08 15:49:36 +0200

    trying to add some stuff

--- headers/Room.h
+++ headers/Room.h
@@ -99,10 +99,10 @@ Room::Room(int a,int b, int c,int d){
         this->doors.emplace_back(new Door(4));
         this->weapons.emplace_back(new FirstWeapon);
         this->weapons.at(0)->updatePos({500.f, 500.f}); //blad przy klikaniu game 
-        sf::Vector2f c;
-        c.x=1200.f;
-        c.y=500.f;
-        this->enemies.emplace_back(new Turret(c));
+        //sf::Vector2f c;
+        //c.x=1200.f;
+        //c.y=500.f;
+        //this->enemies.emplace_back(new Turret(c));
     }
     if(type_id==2){
         this->doors.emplace_back(new Door(1));
